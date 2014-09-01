c6623f9fbe7d04e9c80009acba0bca9341405654
Lisa P <mail@lislis.de>
2014-09-01 23:05:53 +0200

    a little more work

--- app/scripts/lib.js
+++ app/scripts/lib.js
@@ -1,1 +1,48 @@
 // request animation frame
+
+// RequestAnimationFrame 
+//https://gist.github.com/paulirish/1579671
+(function() {
+	var lastTime = 0;
+	var vendors = ['ms', 'moz', 'webkit', 'o'];
+	for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+		window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+		window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame']
+																|| window[vendors[x]+'CancelRequestAnimationFrame'];
+	}
+	if (!window.requestAnimationFrame)
+		window.requestAnimationFrame = function(callback, element) {
+				var currTime = new Date().getTime();
+				var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+				var id = window.setTimeout(function() { callback(currTime + timeToCall); },
+				timeToCall);
+				lastTime = currTime + timeToCall;
+				return id;
+		};
+	if (!window.cancelAnimationFrame)
+		window.cancelAnimationFrame = function(id) {
+				clearTimeout(id);
+		};
+}());
+
+// Get mousecoordinates on canvas
+// https://stackoverflow.com/questions/55677/how-do-i-get-the-coordinates-of-a-mouse-click-on-a-canvas-element
+function relMouseCoords(event){
+	var totalOffsetX = 0;
+	var totalOffsetY = 0;
+	var canvasX = 0;
+	var canvasY = 0;
+	var currentElement = this;
+
+	do{
+			totalOffsetX += currentElement.offsetLeft - currentElement.scrollLeft;
+			totalOffsetY += currentElement.offsetTop - currentElement.scrollTop;
+	}
+	while(currentElement = currentElement.offsetParent)
+
+	canvasX = event.pageX - totalOffsetX;
+	canvasY = event.pageY - totalOffsetY;
+
+	return {x:canvasX, y:canvasY}
+}
+HTMLCanvasElement.prototype.relMouseCoords = relMouseCoords;
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,8 @@
+'use strict';
 
 var Quintessential = function() {
 	this.init();
-}
+};
 
 Quintessential.prototype = {
 
@@ -21,7 +22,23 @@ Quintessential.prototype = {
 		'air',
 		'earth'
 	],
+	currentLevel: '',
 	levelsCompleted: [],
+	canvas: '',
+	canvasElement: document.querySelector('#maingame'),
+	canvasWidth: 320,
+	canvasHeight: 320,
+	rAFid: 0, // requestAnimationFrame id
+	player: {
+		x: 0,
+		y: 0,
+		width: 32,
+		height: 64,
+		speed: 32,
+		spacePressed: false,
+	},
+	enemyNumber: 7,
+	enemies: {},
 
 /*
  * setup canvas,
@@ -29,29 +46,101 @@ Quintessential.prototype = {
  */
 	init: function() {
 		console.log('play');
+		this.actions();
 		// set up canvas 
+		this.canvasElement.width = this.canvasWidth;
+		this.canvasElement.height = this.canvasHeight;
+		this.canvas = this.canvasElement.getContext('2d');
+		// start actual game
 		this.startGame();
 	},
 
 /*
  * assign first game state
- * call draw and update functions
- * -> request animation frame
  */
 	startGame: function() {
+		var self = this;
+		// if no state is chosen
 		if (this.currentState === '') {
-			this.currentState = 'start';
+			this.currentState = 'game';
+			window.location.hash = 'game';
 		}
-		// start gameloop
-		// call draw and update functions
+		this.currentLevel = 'fire';
+		this.player.x = this.canvasWidth / 2 - this.player.width / 2;
+		this.player.y = this.canvasHeight / 2 - this.player.height / 2;
+		this.gameLoop();
+	},
+	gameLoop: function() {
+		var self = this;
+		// for some reason this needs to wrapped to function...
+		this.rAFid = window.requestAnimationFrame(function() {
+			self.gameLoop();
+		});
+		this.gameLogic(); // check parameters for events and call other functions
+		//this.updatePanel(); // update HUB
+		this.draw(); // drawing
+		console.log(new Date());
+	},
+
+/*
+ * THiNK
+ */
+	gameLogic: function() {
+
 	},
 
 /*
  * DRAW
  */
 	draw: function() {
+		this.clear();
 		// check game state
 		// call according draw function
+		if (this.currentState === 'game') {
+			switch(this.currentLevel) {
+				case 'fire':
+					this.drawFire();
+					break;
+				case 'water':
+					this.drawWater();
+					break;
+				case 'air':
+					this.drawAir();
+					break;
+				case 'earth':
+					this.drawEarth();
+					break;
+			}
+		}
+		this.drawPlayer();
+	},
+	drawFire: function() {
+		console.log('drawing fire stage');
+		this.drawElement();
+	},
+	drawWater: function() {
+		console.log('drawing water stage');
+	},
+	drawAir: function() {
+		console.log('drawing air stage');
+	},
+	drawEarth: function() {
+		console.log('drawing earth stage');
+	},
+	drawPlayer: function() {
+		this.canvas.strokeStyle = '#000000';
+		this.canvas.fillStyle = '#000000';
+		// draw outline
+		this.canvas.fillRect(this.player.x, this.player.y, this.player.width, this.player.height);
+	},
+	drawElement: function() {
+		this.canvas.strokeStyle = '#ff0000';
+		this.canvas.fillStyle = '#ff0000';
+		// draw outline
+		this.canvas.fillRect(40, 40, 32, 32);
+	},
+	clear: function() {
+		this.canvas.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
 	},
 
 /*
@@ -82,19 +171,74 @@ Quintessential.prototype = {
 	isState: function(state) {
 		return this.currentState === state;
 	},
+/*
+ * Change STATE helper function
+ */
+	changeState: function() {
+		var self = this;
+		var fragment = window.location.hash;
+		console.log('hello');
+
+		// assign new state
+		if (fragment !== this.currentState) {
+			this.currentState = fragment;
+		}
+		if (this.currentState === 'game') {
+			
+			// this.rAFid = window.requestAnimationFrame(function() {
+			// 	self.gameLoop();
+			// });
+		} else {
+			console.log('Do something about '+ this.currentState);
+		}
+	},
+/*
+ * Choose Level
+ */
+	chooseLevel: function(event) {
+		var levelAttr = event.target.attributes['data-level'];
+		// @todo get level dynamic from link
+		this.currentLevel = 'fire';
+	},
 
 /*
  * CONTROLS
  */
 	actions: function() {
-		// check for game state
-		// add listeners accordingly
-		// remove listenders accordingly
-		// fire left - right
-		// water up - down
-		// air press
-		// earth release
-		// mouseclicks for menu navigation
+		var self = this;
+		// @todo add eventlistener to all elements
+		//var levelLinks = document.querySelectorAll('.level');
+		window.addEventListener('hashChange', this.changeState);
+
+		window.addEventListener('keydown', function(ev) {
+			var key = ev.keyCode;
+			// player controls
+			if (self.currentLevel === 'fire' || self.currentLevel === 'air') {
+				if (key === 38) { // up
+					self.player.y = self.player.y - (1 * self.player.speed);
+				} else if (key === 40) { // down
+					self.player.y = self.player.y + (1 * self.player.speed);
+				}
+			} else if (self.currentLevel === 'water' || 	self.currentLevel === 'earth') {
+				if (key === 39) { // right
+					self.player.x = self.player.x + (1 * self.player.speed);
+				} else if (key === 37) { // left
+					self.player.x = self.player.x - (1 * self.player.speed);
+				}
+			}
+			// collect element switch pt1
+			if (key === 32) { // space
+				this.player.spacePressed = true;
+			}
+		});
+		window.addEventListener('keyup', function(ev) {
+			var key = ev.keyCode;
+			// collect element switch pt2
+			if (key === 32) { // space
+				this.player.spacePressed = false;
+			}
+		});
+		document.querySelector('.level').addEventListener('click', this.chooseLevel);
 	}
 
-}
+};
